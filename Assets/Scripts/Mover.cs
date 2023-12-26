006f3910c7f536a99ef0fa21bec953f832864dbe
harshit1507 <harshit1507@yahoo.com>
2023-12-26 21:39:25 +0530

    Add project files.

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] Transform target;
+
+    Ray lastRay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       // NavMeshAgent agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        }
+
+        Debug.DrawRay(lastRay.origin, lastRay.direction * 100);
+        //GetComponent<NavMeshAgent>().SetDestination(target.position);
+    }
+}
