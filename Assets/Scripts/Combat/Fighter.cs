e582c49bb00ee2c7b3d35acf04b7e6d54613f684
harshit1507 <harshit1507@yahoo.com>
2024-01-04 15:40:46 +0530

    trigger attack animation

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -23,7 +23,13 @@ namespace RPG.Combat
             else
             {
                 GetComponent<Mover>().Cancel();
+                AttackBehaviour();
             }
+        }
+
+        private void AttackBehaviour()
+        {
+            GetComponent<Animator>().SetTrigger("attack");
         }
 
         private bool GetIsInRange()
@@ -40,6 +46,12 @@ namespace RPG.Combat
         public void Cancel()
         {
             this.target = null;
+        }
+
+        //Animation Event
+        private void Hit()
+        {
+
         }
     }
 
